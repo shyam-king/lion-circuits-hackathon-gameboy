a093a2d54fba6dfa54f6002262fe94fee93bcbe2
shyam-king <m.shyam.tnj@gmail.com>
2025-07-26 16:03:33 +0530

    fix

--- gameboy/display.h
+++ gameboy/display.h
@@ -2,11 +2,9 @@
 #define DISPLAY_H
 
 #include <Wire.h>
-#include <stdint.h>
 #include "engine.h"
 
-#define SH1106_ADDR 0x3C // Default I2C address for SH1106
-
+#define SSD1306_ADDR 0x3C // SSD1306 I2C address
 
 // Public interface
 void initSSD1306();
@@ -15,5 +13,7 @@ void writeCommands(const uint8_t *cmds, uint8_t count);
 void writeData(const uint8_t *data, uint8_t count);
 void setCursor(uint8_t page, uint8_t column);
 void applyUpdates(ScreenPageChange *updates, size_t count);
+void clearScreen();
+void drawSmiley();
 
-#endif
+#endif 
