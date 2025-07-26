6e199cc30a619dfe9e41432c23bea8a7060c66ad
Shankrith S <shankrith1618+github@gmail.com>
2025-07-26 15:55:55 +0530

    add test game

--- gameboy/gameboy.ino
+++ gameboy/gameboy.ino
@@ -1,22 +1,44 @@
 #include <Wire.h>
 #include "display.h"
+#include "engine.h"
+#include "test.h"
 
 #define SDA_PIN 8
 #define SCL_PIN 9
 
+GameDisplayState *displayState;
+GameController *spaceship;
+ScreenPageChange *changes;
+
 void setup()
 {
+    displayState = new GameDisplayState();
+    spaceship = new TestGame(displayState);
+    changes = new ScreenPageChange[8*128+1];
+
     Serial.begin(115200);
     Wire.setClock(400000);             // Fast I2C
     Wire.begin();      // Use custom pins
 
     initSSD1306();                     // Initialize SSD1306
     clearScreen();
-    drawSmiley();                // Fill screen with pattern
+    // drawSmiley();                // Fill screen with pattern
 }
 
 
 void loop()
 {
-    // Nothing here yet
+    GameInputState input;
+    GameOutputState output;
+
+    Serial.println(1);
+
+    spaceship -> update_frame(&input, changes, &output);
+
+    Serial.println(2);
+
+    applyUpdates(changes, output.screenPageChanges);
+
+    Serial.println(3);
+    delay(1000);
 }
